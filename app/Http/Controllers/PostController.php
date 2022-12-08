334a1e378267043976be125436a1f2edecf9ba89
ayakaobata <mallowblue.marron@gmail.com>
2022-12-08 18:02:26 +0900

    Add ブログ投稿作成画面

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,9 +16,14 @@ class PostController extends Controller
     }
     
     public function show(Post $post)
-{
+    {
     return view('posts/show')->with(['post' => $post]);
  //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
-}
+    }
+    
+    public function create()
+    {
+    return view('posts/create');
+    }
 }
 ?>
